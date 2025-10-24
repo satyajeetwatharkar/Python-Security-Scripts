9aa794365967c25e1f0a932c2b715085e43ee105
Satyajeet <satyajeetwatharkar@gmail.com>
2025-10-24 11:07:51 -0400

    Day 2

---
+++ week1/Day1/Day 2/script.py
@@ -0,0 +1,63 @@
+print("This is day 2")
+# single line comment
+"""
+Lets 
+see how
+multiline comments work
+"""
+
+x=5 #how cool is it, no need to assign any type just assign
+
+x=int(3) #Ulta Java
+# Variable
+"""
+DOnt have to declare type while intializing, can cast using 
+
+x=str(5)
+x=int(4)
+
+
+type() will give the type of variable
+double quote and single quotes are same
+Variable names are case sensitive
+Var name must start with letter or underscore
+cannot start with no
+only apla numeric or underscore
+
+Can assign val to multiple variables at a time
+
+"""
+print(x)
+
+print(type(x))
+
+y,z,c=10,15,16
+print(y)
+
+x=y=z="orange"
+print(x)
+
+#Unpacking a collection
+
+fruits=["apple","banana","cherry"]
+x=y=z=fruits
+print(x)
+print(y)
+print(z)
+
+# if we have a string we can use + to join diff str, but incase of int it will acts as plus
+
+t="I am testing"
+y="This out "
+
+print(t+y)
+
+# to create a global variable inside a function we can use global keyword
+# random() to generate random no
+#len(a) will provide the length of the string
+
+
+if 1:
+    print("This is testing out if")
+
+#pass is like break
